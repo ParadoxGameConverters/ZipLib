31af8ae61c324ca7584cd73fa2fb155688c44c8c
Ivo Elezovic <ivo@elezovic.su>
2020-01-31 19:37:11 +0100

    Initial depo

---
+++ compression/lzma/detail/lzma_alloc.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifdef _WIN32
+#include "../../../extlibs/lzma/Types.h"
+#endif
+
+#ifdef __linux__
+#include "../../../extlibs/lzma/unix/Types.h"
+#endif
+
+namespace detail
+{
+  class lzma_alloc
+    : public ISzAlloc
+  {
+    public:
+      lzma_alloc()
+      {
+        this->Alloc = [](void*, size_t size)   { return malloc(size); };
+        this->Free  = [](void*, void* address) { free(address); };
+      }
+  };
+}
